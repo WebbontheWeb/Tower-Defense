4e72e430486fa52b00335902d30abac5c6df8d27
Chris Webb <93842126+WebbontheWeb@users.noreply.github.com>
2023-03-20 20:56:27 -0700

    Initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public float startSpeed = 10f;
+
+	[HideInInspector] //because we want to access it elsewhere
+    //but don't want it in inspector
+	public float speed;
+	public float health = 100;
+	public int worth = 50;
+	public GameObject deathEffect;
+
+	void Start()
+	{
+		speed = startSpeed;
+	}
+
+	public void TakeDamage(float amount)
+	{
+		health -= amount;
+
+		if(health <= 0) {
+			Die();
+		}
+	}
+
+	public void Slow(float pct)
+	{
+		speed = startSpeed * (1f - pct);
+	}
+
+	void Die()
+	{
+		PlayerStats.Money += worth;
+
+		GameObject effect = (GameObject)Instantiate(deathEffect, transform.position, Quaternion.identity);
+		Destroy(effect, 5f);
+
+		Destroy(gameObject);
+	}
+
+}
